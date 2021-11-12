03f5c00fdbbafefda3d18bce1a648a268dcaa4a9
ADIL Baig <94189938+projects124@users.noreply.github.com>
2021-11-12 22:49:13 +0500

    Add files via upload
    All operators given in code

---
+++ Logical operators 1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 20:01:31 2021
+
+@author: HOME
+"""
+
+a=3
+b=4
+# Arithmetic operators
+print("The Value of 3+4 is",3+4)
+print("The Value of 3-4 is",3-4)
+print("The Value of 3*4 is",3*4)
+
+print("The Value of 3/4 is",3/4)
+
+# Assignment operators
+a=34
+a-=2
+a*=2
+a/=2
+print (a)
+#Comparison Operators
+#b=(14>=7)
+#b=(14<=7)
+#b=(14<7)
+#b=(14>7)
+b=(14==7)
+print(b)
+
+# Logical Operators
+bool1 = True
+bool2 = False
+print ("The value of bool1 and bool2 is",(bool1 and bool2 ))
+print ("The value of bool1 or  bool2 is",(bool1 or bool2))
+print ("The value of  not bool2 is",(not bool2 ))
