fd5e012745f1af1aa611c640546b5ba92d54e76b
Adil baig <94189938+projects124@users.noreply.github.com>
2022-11-13 23:57:57 +0500

    Add files via upload
    This is some small or short code..

---
+++ 08_pr.py
@@ -0,0 +1,3 @@
+a= input("Enter your number :")
+#a= int(a) #Converted a to an Integer (if possible)
+print(type(a))
---
+++ xzy.py
@@ -0,0 +1,19 @@
+
+
+b=set()
+print(type(b))
+b.add(5)    
+b.add(4)
+b.add(4)
+b.add(4)
+b.add((4,5,6))
+print(b)
+print(len(b))#print the length of this set 
+#Removel of an item
+b.remove(5)#removes 5 from the set (b)
+print(b)
+#print(b.pop)
+print(b)
+
+
+
